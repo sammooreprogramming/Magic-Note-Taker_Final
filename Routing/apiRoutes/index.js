4b2c2d70e3c50bc3cd73fd5adc8291f9e9857606
Sam Moore <sammooresemail23@gmail.com>
2022-08-10 16:01:06 -0700

    added function writeNote to note_functions

--- Routing/apiRoutes/index.js
+++ Routing/apiRoutes/index.js
@@ -2,8 +2,7 @@
 const router = require('express').Router();
 const apiRouting = require('./apiRoutes');
 
-
-//tells the router to use apirouting to route requests.
+//tells the router to use apiRouting to route requests.
 router.use(apiRouting);
 
 module.exports = router;
