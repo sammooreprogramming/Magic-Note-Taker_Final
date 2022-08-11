8b7781d636ef3cd8fc041f5806354174fa09d8a5
Sam Moore <sammooresemail23@gmail.com>
2022-08-11 12:43:54 -0700

    simplified files and reworked server entirely

--- Routing/apiRoutes/index.js
+++
@@ -1,8 +0,0 @@
-// All requirements are met below
-const router = require('express').Router();
-const apiRouting = require('./apiRoutes');
-
-//tells the router to use apiRouting to route requests.
-router.use(apiRouting);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,65 @@
-// All requirements below.
-const express = require("express");
-const apiRoutes = require('./Routing/apiRoutes');
-const htmlRoutes = require('./Routing/htmlRoutes');
+// ================================================================================= //
+// The following set up the basic configuration for express and a server (with PORT).
+// ================================================================================= //
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+const { notes } = require('./db/db.json');
 
-// Express configuration.
-const app  = express();
-const PORT = process.env.PORT || 3001
 
-// Configures express for data parsing.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// This hosts the public folder.
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-require("/api", apiRoutes);
-require("/", htmlRoutes);
+// all necessary configuration functions
+// function that creates a new note is below
+function writeNote (body, notesArray) {
+  const noteData = body
+  notesArray.push(noteData);
 
-// Code to have the server listen.
-app.listen(PORT, function () {
-    console.log(`App live listening on ${PORT}`);
+  fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify({notes: notesArray}, null, 2));
+  return noteData;
+};
+
+// confirms notes by validating them.
+function noteConfirm (note) {
+  if (!note.title || typeof note.title !== 'string') {
+      return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+      return false
+  }
+  return true;
+};
+
+
+// ================= //
+//    API Requests   //
+// ================= //
+
+
+// GET Request
+app.get('/api/notes', function (req, res) {
+  res.json(notes);
+});
+
+
+//POST Request
+app.post('api/notes', function (req, res) {
+ let noteId  = req.body.id;
+ let noteBody = req.body;
+ noteId =notes.length.toString();
+
+ if (!noteConfirm(noteBody)) {
+  res.status(404).send('The page containing the note was not found.');
+ } else { 
+  const newNote = writeNote(noteBody, notes)
+ }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`API server now listening live on port ${PORT}!`);
 });
