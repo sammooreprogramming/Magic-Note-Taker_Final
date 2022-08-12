c3a232c3837c885f625f8146e053f481e356e7dd
Sam Moore <sammooresemail23@gmail.com>
2022-08-11 18:23:13 -0700

    Major update-rearranged files, bug fixes, delete added

--- server.js
+++ server.js
@@ -5,57 +5,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path');
+// will ask for the data in the db folder (the actual notes data).
 const { notes } = require('./db/db.json');
 const { writeNote, noteConfirm } = require('./lib/functions.js');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-// ================= //
-//    API Requests   //
-// ================= //
-
-
-// GET Request
-app.get('/api/notes', function (req, res) {
-  res.json(notes);
-});
-
-
-// POST Request
-app.post('api/notes', function (req, res) {
- let noteId  = req.body.id;
- let noteBody = req.body;
- // ID is created based off the next index's array.
- noteId = notes.length.toString();
-// noteConfirm constant used
- if (!noteConfirm(noteBody)) {
-  res.status(409).send('The note page was in conflict with standards.');
- } else { 
-  // writenote constant used
-  const newNote = writeNote(noteBody, notes)
-  res.json(newNote);
- }
-});
-
-
-// DELETE Request (BONUS Request)
-// first the path to the notes is found to be 'api/notes/:id'
-// then the callback  is used to map and splice the right note away from the array of notes.
-app.delete('api/notes/:id', function (req, res) {
-const clearNote = req.params.id;
-// the new array formed from map() will be accurate to the current note, relatively speaking,
-// after the splice() method is used to delete the note that isn't needed by editing the array and overwriting 1 item via subtraction.
-notes.map((element, index), function () {
-if (clearNote = element.id) {
-  notes.splice(index, 1);
-// returns the JSON data from the element.
-  return res.json(element);
-    }
-  });
-});
-
 
 // This routes to the index.html page.
 app.get('/',  function (req, res) {
@@ -72,7 +28,54 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+
+// ================= //
+//    API Requests   //
+// ================= //
+
+
+// GET Request //
+app.get('/api/notes', function (req, res) {
+  res.json(notes);
+});
+
+
+// POST Request //
+app.post('api/notes', function (req, res) {
+ let noteId  = req.body.id;
+ let noteBody = req.body;
+ // ID is created based off the next index's array.
+ noteId = notes.length.toString();
+// noteConfirm constant used
+ if (noteConfirm(noteBody)) {
+   // writenote constant used
+   const newNote = writeNote(noteBody, notes)
+   res.json(newNote);
+ } else { 
+  // return error if necessary.
+  res.status(409).send('The note page was in conflict with webpage standards.');
+ }
+});
+
+
+// DELETE Request (BONUS Request) //
+// first the path to the notes is found to be 'api/notes/:id'
+// then the callback  is used to map and splice the right note away from the array of notes.
+app.delete('api/notes/:id', function (req, res) {
+const clearNote = req.params.id;
+// the new array formed from map() will be accurate to the current note, relatively speaking,
+// after the splice() method is used to delete the note that isn't needed by editing the array and overwriting 1 item via subtraction.
+notes.map((element, index), function () {
+if (clearNote = element.id) {
+  notes.splice(index, 1);
+// returns the JSON data from the element.
+  return res.json(element);
+    }
+  });
+});
+
+
 // This actually deploys the listener for the server and runs it.
 app.listen(PORT, () => {
-  console.log(`API server now listening live on port ${PORT}!`);
+  console.log(`API server now live and listening on ${PORT}`)
 });
