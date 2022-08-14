ff44e667f2f509e3c3e34cb8d9858334760642fd
Sam Moore <sammooresemail23@gmail.com>
2022-08-13 19:48:53 -0700

    heroku push

--- server.js
+++ server.js
@@ -4,15 +4,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoute = require("./routes/apiRoutes");
-const htmlRoute = require("./routes/htmlRoutes");
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use("/api", apiRoute);
-app.use("/", htmlRoute);
+app.use("/api", "./routes/apiRoutes");
+app.use("/", "./routes/htmlRoutes");
 
 // This actually deploys the listener for the server and runs it.
 app.listen(PORT, () => {
