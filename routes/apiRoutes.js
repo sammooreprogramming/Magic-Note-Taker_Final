16073e0cbf4099c2187efe0a26c0631c27a55b9d
Sam Moore <sammooresemail23@gmail.com>
2022-09-29 16:19:53 -0700

    revised DELETE BONUS route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -30,19 +30,15 @@ res.json(notes).catch(err => {
   
 
   // DELETE Request (BONUS Request) //
-  
+
   // first the path to the notes is found to be 'api/notes/:id'
   // then the callback  is used to map and splice the right note away from the array of notes.
-  router.delete('api/notes/:id', function (req, res) {
-  const clearNote = req.params.id;
-  // the new array formed from map() will be accurate to the current note, relatively speaking,
-  // after the splice() method is used to delete the note that isn't needed by editing the array and overwriting 1 item via subtraction.
-  notes.map((element, index), function () {
-  if (clearNote = element.id) {
-    notes.splice(index, 1);
-  // returns the JSON data from the element.
-    return res.json(element);
-      };
+  router.delete('/notes/:id', function (req, res) {
+
+    store.removeNote(req.params.id).then(function () {
+      res.json({  ok: true }).catch(err => {
+        res.status(500).json(err);
+      });
     });
   });
 
